710b29607bef5d03a48ae5818c81c8691fa5ab71
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2023-09-18 15:39:40 -0300

    Att py

--- Relatorio/testeL.py
+++ Relatorio/testeL.py
@@ -20,3 +20,26 @@ print(f'O valor formatado da célula é: {valor_celula}')
 
 #----------------------------------------------------------------------------------------------
 
+
+import openpyxl
+
+# Abra o arquivo Excel
+workbook = openpyxl.load_workbook('Relatorio\REVISÃO - Boletim de Amostragem 7918.xlsx')  # Substitua 'exemplo.xlsx' pelo nome do seu arquivo Excel
+
+# Selecione a planilha desejada
+planilha = workbook['PMN-03']  # Substitua 'Planilha1' pelo nome da sua planilha
+
+# Acesse a célula desejada (por exemplo, A1)
+célula = planilha['E41']  # Substitua 'A1' pela célula que você deseja
+
+# Forçar o cálculo da fórmula na célula
+workbook.calculation.on_change(célula)
+
+# Obtenha o valor da célula, que é o resultado da fórmula
+valor_celula = célula.value
+
+# Feche o arquivo Excel
+workbook.close()
+
+# Imprima o valor da célula
+print(f'O valor da célula é: {valor_celula}')
