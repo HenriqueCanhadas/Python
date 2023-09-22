facab2f5f667e0834ddc8543ca03181726892504
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2023-09-22 15:27:42 -0300

    Janela Operante

--- Janela/TesteJanela.py
+++ Janela/TesteJanela.py
@@ -3,6 +3,8 @@ from tkinter import filedialog
 import xlwings as xw
 import openpyxl
 import pandas as pd
+from openpyxl.utils.dataframe import dataframe_to_rows
+from copy import deepcopy
 
 # Variável global para armazenar o caminho do arquivo Excel
 file_path = ""
@@ -16,26 +18,24 @@ def carregar_excel():
         dataframe = pd.read_excel(file_path)
         status_label.config(text="Arquivo Excel carregado com sucesso!")
 
+
 # Função para rodar o programa
 def rodar_programa():
-    
-
     global file_path  # Acessa a variável global file_path
+    global valores_xlwings
+    global valores_workbook_openpyxl
+    global workbook_openpyxl
 
     wb_xlwings = xw.Book(file_path)
+
     valores_xlwings = []
-
-    #linha=69
-    # Uma lista de números
-    numeros = [1, 2, 3, 4, 5]
-
-# Loop for para iterar sobre os números e imprimir cada um
-    for numero in numeros:
-        print(numero)
+    valores_workbook_openpyxl = []
 
     for sheet_name in wb_xlwings.sheets:
 
         planilha = wb_xlwings.sheets[sheet_name]
+
+        planilha.activate()
 
         condutividade = planilha.range('E41').value
         oxirreducao = planilha.range('G41').value
@@ -54,31 +54,97 @@ def rodar_programa():
         print(f'Temperatura: {temperatura}')
         print(f'Turbidez: {turbidez}')
         print("-" * 100)
-    
+
     wb_xlwings.close()
 
-    if file_path:
-        # Agora você pode usar a variável file_path aqui na função rodar_programa
-        print(f'Caminho do arquivo Excel: {file_path}')
-        # Resto do seu código da função rodar_programa 
+    workbook_openpyxl = openpyxl.load_workbook(file_path)
 
-# Função para salvar o arquivo Excel no local de origem
+    for sheet_name in workbook_openpyxl.sheetnames:
+        planilha_openpyxl = workbook_openpyxl[sheet_name]
+
+        identificacao = planilha_openpyxl['C30'].value
+        data = planilha_openpyxl['C6'].value
+        horaensaio = planilha_openpyxl['H30'].value
+        horaamostragem = planilha_openpyxl['K30'].value
+        condicoes = planilha_openpyxl['P30'].value
+
+        valores_workbook_openpyxl.append((identificacao, data, horaensaio, horaamostragem, condicoes))
+
+
+        print(f'Identificação/Aba/Guia: {identificacao}')
+        print(f'Data: {data}')
+        print(f'Hora do ensaio: {horaensaio}')
+        print(f'Hora da amostragem: {horaamostragem}')
+        print(f'Condições ambientais: {condicoes}')
+
+        print("-" * 100)
+
+    status_label.config(text="Todos os dados foram Armazenados")
+
+    print('Todos os dados foram Armazenados')
+
+    print("-" * 100)
+
+
+
 def salvar_excel():
-    if 'dataframe' in globals():
+    global valores_xlwings
+    global valores_workbook_openpyxl
+    global workbook_openpyxl
+    global file_path
+    global status_label
+
+    if file_path:
+        # Abrir o arquivo Excel original
+        print(f"Abrindo o arquivo Excel existente em: {file_path}")
+        workbook_original = openpyxl.load_workbook(file_path)
+
+        # Criar uma cópia exata do arquivo Excel original
+        workbook_copia = deepcopy(workbook_original)
+
+        # Acesse a planilha 'FINAL' na cópia (ou ajuste conforme necessário)
+        sheet = workbook_copia.active
+        sheet.title = 'FINAL'
+
+        linha = 69
+
+        while len(valores_xlwings) > 0 and len(valores_workbook_openpyxl) > 0:
+            if len(valores_xlwings) > 0:
+                condutividade, oxirreducao, oxigenio, ph, temperatura, turbidez = valores_xlwings.pop(0)
+
+                sheet[f'E{linha}'] = condutividade
+                sheet[f'F{linha}'] = oxirreducao
+                sheet[f'G{linha}'] = oxigenio
+                sheet[f'H{linha}'] = ph
+                sheet[f'I{linha}'] = temperatura
+                sheet[f'J{linha}'] = turbidez
+            
+            if len(valores_workbook_openpyxl) > 0:
+                identificacao, data, horaensaio, horaamostragem, condicoes = valores_workbook_openpyxl.pop(0)
+
+                sheet[f'A{linha}'] = identificacao
+                sheet[f'B{linha}'] = data
+                sheet[f'C{linha}'] = horaensaio
+                sheet[f'D{linha}'] = horaamostragem
+                sheet[f'K{linha}'] = condicoes
+            
+            linha += 1
+
+        # Salvar o arquivo com um novo nome
         save_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Arquivos Excel", "*.xlsx *.xls")])
         if save_path:
-            dataframe.to_excel(save_path, index=False)
-            status_label.config(text="Arquivo Excel salvo com sucesso!")
+            workbook_copia.save(save_path)
+            status_label.config(text="Novo arquivo Excel salvo com sucesso!")
 
-# Resto do seu código permanece o mesmo
+        print("Processo concluído com sucesso.")
+
 
 # Criação da janela principal
 root = tk.Tk()
-root.title("Manipulação de Arquivo Excel")
+root.title("Nome Do Programa")
 
 # Definir tamanho mínimo para a janela (width x height)
 root.minsize(250, 250)  # Ajuste os valores de largura e altura conforme necessário
-
 
 # Botão para carregar o Excel
 load_button = tk.Button(root, text="Carregar Excel", command=carregar_excel)
