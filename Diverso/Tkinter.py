6ab70c63cc8af1e40a2ed9b0c53f31429be40575
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2023-10-11 13:44:30 -0300

    Tkinter

---
+++ Diverso/Tkinter.py
@@ -0,0 +1,19 @@
+import customtkinter
+
+janela=customtkinter.CTk()
+janela.geometry("250x250")
+janela.title("SERVMAR")
+
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+
+
+
+
+
+
+
+
+janela.mainloop()
