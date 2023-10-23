5a4dac41450206f9a0eea9b8aa8142cc0fd0266e
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2023-10-23 13:40:39 -0300

    Att pivot

--- Diverso/Arquivo001.py
+++ Diverso/Arquivo001.py
@@ -8,3 +8,15 @@ import uuid
 
 token = str(uuid.uuid4())  # Gera um token UUID aleatório
 print(token)
+
+# Suponha que você tenha uma lista chamada 'lista_origem'
+lista_origem = ['item1', 'item2', 'item3']
+
+# Crie uma nova lista chamada 'nova_lista' e adicione o valor de 'item2' a ela
+nova_lista = []
+valor_a_copiar = lista_origem[1]  # índice 1 corresponde a 'item2'
+nova_lista.append(valor_a_copiar)
+
+print(lista_origem)
+print(valor_a_copiar)
+print(nova_lista)
--- TestePivot/Ler.py
+++ TestePivot/Ler.py
@@ -6,10 +6,7 @@ workbook = openpyxl.load_workbook('TestePivot/Export_Toyota_SBC_Geral.xlsx')
 
 sheet = workbook['Amostras Resultados (2)']
 
-
-
-def case_1():
-    '''
+''' 
     def printLinhaLaboratorio(linha):
         for linha in sheet.iter_rows(min_row=linha, max_row=linha):
             for cell in linha:
@@ -28,78 +25,75 @@ def case_1():
                 contador += 1
 
     print("Total:",contador)
-    '''
-
-
-    #print("A célula atual é igual à próxima célula")
-
-def case_2(row):
-    def case_2_inner():
-        #print("Celula anterior:", cell1)
-        #print (sheet.cell(row=row, column=1))
-
-        global id
-        global tipo
-        global data
-
-
-        id.append(sheet.cell(row=row, column=1).value)
-        tipo.append(sheet.cell(row=row, column=2).value)
-        data.append(sheet.cell(row=row, column=3).value)
-        
-        #print("A célula atual é diferente da próxima célula")
-        
-        #print (sheet.cell(row=row +1, column=1))
-        #print("Celula seguinte:", cell2)
-        #print (sheet.cell(row=row+1, column=1))
-
-        #!!!! Ele esta pegando o ultimo e nao atualizando o penultimo
-    
-    
-    return case_2_inner  # Retorna a função interna sem argumentos
-
-def default_case():
-    print("Caso padrão")
+'''
 
 id=[]
 tipo=[]
 data=[]
 
+
+
 column_number = 1  # Número da coluna desejada (1 para A, 2 para B, etc.)
-row = 1
 
-for row in range(1, sheet.max_row):
-    cell1 = sheet.cell(row=row, column=1).value
-    cell2 = sheet.cell(row=row + 1, column=1).value
+for row in range(1, sheet.max_row + 1):  # Note o "+ 1" para incluir a última linha.
+    cell1 = sheet.cell(row=row, column=column_number).value
+    cell2 = sheet.cell(row=row + 1, column=column_number).value
 
-    print(cell2)
-
-    if cell1 is not None and cell2 is not None:
-        switch = {
-            cell1 == cell2: case_1,
-            cell1 != cell2: case_2(row)
-        }
-        switch.get(True, default_case)()
-
+    if cell1 is not None and (cell2 is None or cell1 != cell2):
+        # Adicionar os valores às listas quando a próxima célula for nula ou diferente
+        id.append(sheet.cell(row=row, column=1).value)
+        tipo.append(sheet.cell(row=row, column=2).value)
+        data.append(sheet.cell(row=row, column=3).value)
 
 
 print("\n",id)
 print("\n",tipo)
 print("\n",data)
 
+result_list = []
 
+
+for row in range(1, sheet.max_row + 1):  # Note o "+ 1" para incluir a última linha.
+    cell1 = sheet.cell(row=row, column=column_number).value
+    cellD = sheet.cell(row=row, column=4).value  # Valor da coluna 'D'
+    cellE = sheet.cell(row=row, column=5).value  # Valor da coluna 'E'
+    cellF = sheet.cell(row=row, column=6).value  # Valor da coluna 'F'
+
+    if cell1 is not None and (cellD is not None or cellE is not None or cellF is not None):
+        # Adicionar os valores à lista id no formato desejado
+        id.append([cell1, [cellD, [cellE, cellF]]])
+
+print(id, end="\n")
+
+
+
+
+'''
+for item_id in id:
+    for row in range(min_col=5, max_col=5,min_row=1, max_row=sheet.max_row):
+            for cell in row:
+                if cell.value is not None:
+                        
+                        
+                        # Suponha que 'item_id' seja o valor da coluna A (por exemplo, 'PM-123')
+                        col_D_value = sheet.cell(row=row, column=4).value  # Valor da coluna D
+                        col_E_value = sheet.cell(row=row, column=5).value  # Valor da coluna E
+                        col_F_value = sheet.cell(row=row, column=6).value  # Valor da coluna F
+                        # Crie a estrutura de lista aninhada e adicione-a a 'result_list'
+'''
+
+
+# Agora, 'result_list' contém a estrutura desejada com os valores das colunas 'D', 'E' e 'F' para cada item em 'id'.
 
 '''
 for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, min_col=1, max_col=sheet.max_column):
              for cell in row:
                 print(print(cell.value, end=' '))
+
+
+  nested_list = [item_id, [col_D_value, col_E_value, col_F_value]]
+    result_list.append(nested_list)
 '''
-
-
-
-cell = sheet['A1'].value
-
-print("\n",cell)
 
 print(f'Valores lidos em {sheet}:')
 
