115a7ead78937106b0a279edd8c9b9769254b5e0
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2023-10-26 15:29:03 -0300

    att

--- TestePivot/Tabela Cetesb/teste.py
+++ TestePivot/Tabela Cetesb/teste.py
@@ -2,16 +2,75 @@ import openpyxl
 
 workbook = openpyxl.load_workbook('TestePivot/Tabela Cetesb/arquivoteste.xlsx')
 
-sheet = workbook['Table 2']
-'''
+sheet = workbook['Tabela']
+
+teste = []
+
+coordenada = []
+
+nomes = [
+    "INORGÂNICOS",
+    "HIDROCARBONETOS AROMÁTICOS VOLÁTEIS",
+    "HIDROCARBONETOS POLICÍCLICOS AROMÁTICOS",
+    "BENZENOS CLORADOS",
+    "ETANOS CLORADOS",
+    "ETENOS CLORADOS",
+    "METANOS CLORADOS",
+    "FENÓIS CLORADOS",
+    "FENÓIS NÃO CLORADOS",
+    "ÉSTERES FTÁLICOS",
+    "PESTICIDAS",
+    "OUTROS",
+    "VRQ – Valor de Referência de Qualidade; VP – Valor de Prevenção; VI – Valor de Intervenção"
+]
+
+
 # Percorra as células da planilha e imprima o conteúdo se não estiver vazia
 for row in sheet.iter_rows():
     for cell in row:
         if cell.value is not None:
-            print(cell.value, end='\t')  # Imprime o valor da célula separado por tabulação
-    print()  # Imprime uma nova linha após cada linha de células
-'''
+            #print(cell.value, end='\t')  # Imprime o valor da célula separado por tabulação
+            pass
+        if cell.value in nomes:
+               teste.append(cell.value)
+               coordenada.append(cell.coordinate)
 
+print(teste)
+print(coordenada)
+
+intervalos = []
+
+# Inicialize variáveis para rastrear o intervalo atual
+inicio_intervalo = None
+
+# Itere sobre os valores
+for valor in coordenada:
+    # Extraia as colunas e linhas
+    coluna, linha = valor[0], int(valor[1:])
+    
+    # Verifique se o intervalo atual começou
+    if inicio_intervalo is None:
+        inicio_intervalo = (coluna, linha)
+    # Verifique se o intervalo atual terminou
+    elif linha > inicio_intervalo[1] + 1:
+        coluna_atual, linha_atual = inicio_intervalo
+        while linha_atual < linha:
+            cell = sheet[f'{coluna_atual}{linha_atual}']
+            print(cell.value)
+            linha_atual += 1
+        inicio_intervalo = (coluna, linha)
+
+# Obtenha o número da última célula da lista
+ultima_celula = coordenada[-1]
+linha_ultima_celula = int(ultima_celula[1:])
+
+# Imprima os valores da última linha
+while linha_ultima_celula <= int(sheet[-1][1:]):
+    cell = sheet[f'{coluna}{linha_ultima_celula}']
+    print(cell.value)
+    linha_ultima_celula += 1
+
+''''
 # Especifique a coluna que você deseja ler
 coluna = 'O'  # Substitua 'A' pela letra da coluna que você deseja ler
 
@@ -25,7 +84,7 @@ valores_coluna = []
 while True:
     celula = sheet[f'{coluna}{linha_inicial}']
     valor = celula.value
-    if valor is None or valor == 'NONE':
+    if valor is None:
         break
     valores_coluna.append(valor)
     linha_inicial += 1
@@ -34,7 +93,7 @@ while True:
 print("Valores da coluna:")
 for valor in valores_coluna:
     print(valor)
-
+'''
 
 # Feche o arquivo após a leitura
 workbook.close()
