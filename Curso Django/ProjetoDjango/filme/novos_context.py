3756c8f53e6d6c054d1cb1e431be37176595e135
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2024-05-13 09:12:28 -0300

    att aula 31

--- Curso Django/ProjetoDjango/filme/novos_context.py
+++ Curso Django/ProjetoDjango/filme/novos_context.py
@@ -7,3 +7,7 @@ def lista_filmes_recentes(request):
 def lista_filmes_emalta(request):
     lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:10]
     return {"lista_filmes_emalta" : lista_filmes}
+
+def filme_destaque(request):
+    filme = Filme.objects.order_by('-data_criacao')[0]
+    return {"filme_destaque" : filme}
--- Curso Django/ProjetoDjango/hashflix/settings.py
+++ Curso Django/ProjetoDjango/hashflix/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'filme.novos_context.lista_filmes_recentes',
                 'filme.novos_context.lista_filmes_emalta',
+                'filme.novos_context.filme_destaque',
             ],
         },
     },
