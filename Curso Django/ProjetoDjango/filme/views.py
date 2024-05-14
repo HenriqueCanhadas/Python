f4555cf632116b18911664617cd9538893735928
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2024-05-14 16:06:25 -0300

    att aula 36

--- Curso Django/ProjetoDjango/filme/views.py
+++ Curso Django/ProjetoDjango/filme/views.py
@@ -27,6 +27,8 @@ class Detalhesfilme(DetailView):
         filme = self.get_object()
         filme.visualizacoes += 1
         filme.save()
+        usuario = request.user
+        usuario.filmes_vistos.add(filme)
         return super().get(request, *args, **kwargs)
 
 
